486180f544a45186d1adef8eeff1b9d4157f36a0
itsolver <angus@itsolver.net>
2023-05-25 15:02:14 +1000

    To run both monitor1.py and monitor2.py in parallel. You can use the start command with the /B option, which launches the specified command in the background without opening a new command prompt window. This will allow both scripts to run simultaneously.

--- run_automation.bat
+++ run_automation.bat
@@ -5,5 +5,5 @@ cd C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards
 git pull
 
 echo Running automation script...
-start "" "C:\Users\dashboard-kiosk\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards\monitor1.py"
-start "" "C:\Users\dashboard-kiosk\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards\monitor2.py"
+start /B "" "C:\Users\dashboard-kiosk\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards\monitor2.py"
+start /B "" "C:\Users\dashboard-kiosk\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards\monitor1.py"

df198a4c0883aa970d5bb6333d245ec756adf43c
itsolver <angus@itsolver.net>
2023-07-31 13:03:55 +1000

    don't show taskkills output and keep cmd window open

--- run_only_monitor2.bat
+++ run_only_monitor2.bat
@@ -1,9 +1,9 @@
 @echo off
 
 : close all chrome and cmd instances
-taskkill /f /im chrome.exe
-taskkill /f /im chromedriver.exe
-taskkill /f /im cmd.exe /fi "pid ne %%"
+taskkill /f /im chrome.exe >nul 2>&1
+taskkill /f /im chromedriver.exe >nul 2>&1
+taskkill /f /im cmd.exe /fi "pid ne %%" >nul 2>&1
 
 echo Syncing local repository with GitHub...
 cd C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards
@@ -11,4 +11,4 @@ git pull
 ping 127.0.0.1 -n 5 > nul
 
 echo Running automation script...
-start "" cmd /c "C:\Users\dashboard-kiosk\AppData\Local\Programs\Python\Python311\python.exe C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards\monitor2.py"
+start "" cmd /k "C:\Users\dashboard-kiosk\AppData\Local\Programs\Python\Python311\python.exe C:\Users\dashboard-kiosk\AppData\Roaming\combined-metal-roofing-dashboards\monitor2.py"
